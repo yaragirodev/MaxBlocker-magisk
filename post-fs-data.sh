a13e9d3fbba023c38654db2d5b1297914903b1ca
Yaroslav <116502429+yaragirodev@users.noreply.github.com>
2025-08-17 22:20:48 +0300

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -6,5 +6,5 @@ mkdir -p /data/local/tmp
 # Блокируем приложение сразу после монтирования файловой системы
 if pm list packages | grep -q "ru.oneme.app"; then
     pm disable-user ru.oneme.app 2>/dev/null
-    echo "$(date): MAX app disabled at boot" >> /data/local/tmp/max_blocker.log
+    echo "$(date): MAX disabled at boot" >> /data/local/tmp/max_blocker.log
 fi
